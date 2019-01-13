d7dd33f8ed6bd125359b4e011e3204b10d45c5a1
TJ Draper <tj@buzzingpixel.com>
2019-01-13 15:11:56 -0600

    Add a docker environment for testing

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2019 BuzzingPixel, LLC
+ * @license Apache-2.0
+ */
+
+/**
+ * This file is for testing purposes only
+ */
+
+use corbomite\di\Di;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+var_dump('here');
+die;
